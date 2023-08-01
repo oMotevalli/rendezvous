ea9e85d0ef38a137e6afb2009fcdb51677aa3bfb
Olivia Motevalli <olivia.motevalli.aliabadi@gmail.com>
2023-08-01 17:05:15 +0100

    begin rendez app

--- App.js
+++ App.js
@@ -1,10 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, TextInput, Button } from "react-native";
+import Home from "./src/screens/Home";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Home />
+      <Text style={styles.textStyle}>Rendezvous</Text>
+      <TextInput style={styles.inputStyle} placeholder="Username"></TextInput>
+      <TextInput style={styles.inputStyle} placeholder="Password"></TextInput>
+      <Button color={"#573295"} title={"Login"} />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +18,19 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#ffafcc",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  textStyle: {
+    fontSize: 50,
+    color: "#fff",
+    fontFamily: "monospace",
+  },
+  inputStyle: {
+    margin: 20,
+    padding: 10,
+    width: "80%",
+    backgroundColor: "#fff",
   },
 });
---
+++ src/screens/Home.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Text, View, StyleSheet } from "react-native";
+
+const Home = () => {
+  return;
+  <View>
+    <Text style={styles.textStyle}>Rendezvous</Text>;
+  </View>;
+};
+
+const styles = StyleSheet.create({
+  textStyle: {
+    fontSize: 30,
+  },
+});
+
+export default Home;
